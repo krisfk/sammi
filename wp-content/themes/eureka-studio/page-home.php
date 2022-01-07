14c2160beba85aa4c5a3c5d470985d26a2578e1c
kaiyu li <krisfk@gmail.com>
2022-01-08 05:25:57 +0800

    .

--- wp-content/themes/eureka-studio/page-home.php
+++ wp-content/themes/eureka-studio/page-home.php
@@ -100,9 +100,18 @@ get_header();
     <h2 class="text-center">WORKS</h2>
 
     <div class="row">
-        <div class="col-4"></div>
-        <div class="col-4"></div>
-        <div class="col-4"></div>
+        <div class="col-4"><a href="#"><img class="w-100"
+                    src="http://64.227.13.14/sammi/wp-content/uploads/2022/01/work-img.jpg" alt=""></a>
+            <div>Lorem ipsum dolor sit amet, </div>
+        </div>
+        <div class="col-4"><a href="#"><img class="w-100"
+                    src="http://64.227.13.14/sammi/wp-content/uploads/2022/01/work-img.jpg" alt=""></a>
+            <div>Lorem ipsum dolor sit amet, </div>
+        </div>
+        <div class="col-4"><a href="#"><img class="w-100"
+                    src="http://64.227.13.14/sammi/wp-content/uploads/2022/01/work-img.jpg" alt=""></a>
+            <div>Lorem ipsum dolor sit amet, </div>
+        </div>
     </div>
 
 </div>
