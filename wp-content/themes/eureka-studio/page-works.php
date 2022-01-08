7fb0140ae9b16e2ba70aa7c43657e514a3211c5b
kaiyu li <krisfk@gmail.com>
2022-01-09 02:05:54 +0800

    .

--- wp-content/themes/eureka-studio/page-works.php
+++ wp-content/themes/eureka-studio/page-works.php
@@ -116,52 +116,7 @@ get_header();
         </div>
     </div>
 
-    <div class="text-center"><a href="#" class="more-btn">MORE</a></div>
 
-
-
-    <div class="mt-5 d-table"></div>
-    <div class="mt-5">
-        <h2 class="text-center">OUR CLIENTS</h2>
-        <div class="mt-5 container d-flex align-items-center flex-wrap">
-
-            <a href="" class="client-logo-a"><img class="w-100"
-                    src="http://64.227.13.14/sammi/wp-content/uploads/2022/01/client-logo-1.png" alt=""></a>
-
-            <a href="" class="client-logo-a"><img class="w-100"
-                    src="http://64.227.13.14/sammi/wp-content/uploads/2022/01/client-logo-1.png" alt=""></a>
-            <a href="" class="client-logo-a"><img class="w-100"
-                    src="http://64.227.13.14/sammi/wp-content/uploads/2022/01/client-logo-1.png" alt=""></a>
-            <a href="" class="client-logo-a"><img class="w-100"
-                    src="http://64.227.13.14/sammi/wp-content/uploads/2022/01/client-logo-1.png" alt=""></a>
-            <a href="" class="client-logo-a"><img class="w-100"
-                    src="http://64.227.13.14/sammi/wp-content/uploads/2022/01/client-logo-1.png" alt=""></a>
-            <a href="" class="client-logo-a"><img class="w-100"
-                    src="http://64.227.13.14/sammi/wp-content/uploads/2022/01/client-logo-1.png" alt=""></a>
-
-            <a href="" class="client-logo-a"><img class="w-100"
-                    src="http://64.227.13.14/sammi/wp-content/uploads/2022/01/client-logo-1.png" alt=""></a>
-            <a href="" class="client-logo-a"><img class="w-100"
-                    src="http://64.227.13.14/sammi/wp-content/uploads/2022/01/client-logo-1.png" alt=""></a>
-            <a href="" class="client-logo-a"><img class="w-100"
-                    src="http://64.227.13.14/sammi/wp-content/uploads/2022/01/client-logo-1.png" alt=""></a>
-            <a href="" class="client-logo-a"><img class="w-100"
-                    src="http://64.227.13.14/sammi/wp-content/uploads/2022/01/client-logo-1.png" alt=""></a>
-            <a href="" class="client-logo-a"><img class="w-100"
-                    src="http://64.227.13.14/sammi/wp-content/uploads/2022/01/client-logo-1.png" alt=""></a>
-
-            <a href="" class="client-logo-a"><img class="w-100"
-                    src="http://64.227.13.14/sammi/wp-content/uploads/2022/01/client-logo-1.png" alt=""></a>
-            <a href="" class="client-logo-a"><img class="w-100"
-                    src="http://64.227.13.14/sammi/wp-content/uploads/2022/01/client-logo-1.png" alt=""></a>
-            <a href="" class="client-logo-a"><img class="w-100"
-                    src="http://64.227.13.14/sammi/wp-content/uploads/2022/01/client-logo-1.png" alt=""></a>
-            <a href="" class="client-logo-a"><img class="w-100"
-                    src="http://64.227.13.14/sammi/wp-content/uploads/2022/01/client-logo-1.png" alt=""></a>
-
-
-        </div>
-    </div>
 </div>
 
 
