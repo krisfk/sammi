12b6064eedbb50b872e8b6f111b3ca226d25673f
kaiyu li <krisfk@gmail.com>
2022-01-12 23:21:38 +0800

    .

--- wp-content/themes/eureka-studio/index.php
+++ wp-content/themes/eureka-studio/index.php
@@ -17,9 +17,9 @@
 get_header(); ?>
 
 <?php if ( is_home() && ! is_front_page() && ! empty( single_post_title( '', false ) ) ) : ?>
-	<header class="page-header alignwide">
-		<h1 class="page-title"><?php single_post_title(); ?></h1>
-	</header><!-- .page-header -->
+<header class="page-header alignwide">
+    <h1 class="page-title"><?php single_post_title(); ?></h1>
+</header><!-- .page-header -->
 <?php endif; ?>
 
 <?php
--- wp-content/themes/eureka-studio/page-home.php
+++ wp-content/themes/eureka-studio/page-home.php
@@ -110,7 +110,7 @@ get_header();
                         src="http://64.227.13.14/sammi/wp-content/uploads/2022/01/work-img.jpg" alt=""></a>
                 <div>Lorem ipsum dolor sit amet, </div>
             </div>
-            <div class="col-4 mb-4"><a href="#" class="work-a"><img class="w-100"
+            <!-- <div class="col-4 mb-4"><a href="#" class="work-a"><img class="w-100"
                         src="http://64.227.13.14/sammi/wp-content/uploads/2022/01/work-img.jpg" alt=""></a>
                 <div>Lorem ipsum dolor sit amet, </div>
             </div>
@@ -129,7 +129,7 @@ get_header();
             <div class="col-4 mb-4"><a href="#" class="work-a"><img class="w-100"
                         src="http://64.227.13.14/sammi/wp-content/uploads/2022/01/work-img.jpg" alt=""></a>
                 <div>Lorem ipsum dolor sit amet, </div>
-            </div>
+            </div> -->
         </div>
     </div>
 
