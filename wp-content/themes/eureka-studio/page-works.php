78ec204ddafbc28969f2464c52d287bba16084fe
kaiyu li <krisfk@gmail.com>
2022-01-13 23:42:57 +0800

    .

--- wp-content/themes/eureka-studio/page-works.php
+++ wp-content/themes/eureka-studio/page-works.php
@@ -161,6 +161,7 @@ $(function() {
 
         $('.work-col').fadeOut(0);
 
+        alert(filter_type_arr.length);
         if (filter_type_arr.length == 0) {
             for (i = 0; i < filter_type_arr.length; i++) {
                 $('.work-col').fadeIn(0);
