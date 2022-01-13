899cb5048cdc89de404bc714877449854bd6936c
kaiyu li <krisfk@gmail.com>
2022-01-13 21:15:10 +0800

    .

--- wp-content/themes/eureka-studio/page-home.php
+++ wp-content/themes/eureka-studio/page-home.php
@@ -176,7 +176,7 @@ get_header();
         <div class="mt-lg-5 mt-md-5 mt-sm-4 mt-4 container">
 
 
-            <div class="row">
+            <div class="row justify-content-center">
                 <?php
         
         $query_args = array(
